624198e2544f7ac8d7e24fc116dab25d023abcbd
Rishav Dobriyal <73049840+Rishav09835@users.noreply.github.com>
2024-10-07 12:48:29 +0000

    basic_datatypes

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //to treat all js code as newer version.
+
+//ECMA script - writes standards of js. (original standard)
+//mdn= to read documentation about js
+// alert("Hello there"); //we are working inside node.js not in website.
+
+let course = "BTECH"
+let age = 21
+let logout = false
+
+//primitive data types
+//number = 2^53 or 52
+//bigInt (for very large value)
+// string = ""
+// boolean =  true,false
+//null = standalone value 
+//undefined = value is not given but variable is defined.
+// symbo = uniqueness
+
+
+//object
+
+// console.log(typeof undefined); //undefined
+//console.log(typeof null); //object
