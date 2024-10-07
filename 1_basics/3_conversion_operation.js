e7c78f2afd6b1766cd5ab382f24d9b52b2aeda2a
Rishav Dobriyal <73049840+Rishav09835@users.noreply.github.com>
2024-10-07 13:08:29 +0000

    conversionofdatatype

---
+++ 1_basics/3_conversion_operation.js
@@ -0,0 +1,22 @@
+// "33" = 33;
+// let score = 33
+// let scoretoInt = Number(score)
+// console.log(scoretoInt);
+// console.log(typeof (scoretoInt));
+
+// "33xyz" = NaN, type is returned as number;
+let score = "33xyz"
+let scoretoInt = Number(score)
+// console.log(scoretoInt);
+// console.log(typeof (scoretoInt));
+// similarly
+// true = 1, false = 0
+// null = 0,typeof = number
+// undefined = Nan, typeof = number
+
+let loggedin = 1;
+let convertbool = Boolean(loggedin);
+// console.log(convertbool);
+//1->true, 2-> false;
+//empty string "" = false;
+//"rvd" = true;
